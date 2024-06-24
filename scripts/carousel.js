9e5d253d203eb32d58d26833b00e3cd31f8d532c
niccolosegato <segato117@gmail.com>
2024-06-24 07:00:48 +0200

    New working casee history carousel

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,18 +1,36 @@
-const images = ["services-1.jpg", "services-2.jpg"];
-let currentImage = 0;
+// From 1 to 33
+let currentImage = 1;
 
 function nextCarousel() {
     const carousel = document.getElementById('ch-sub');
     const carouselTitle = document.getElementById('ch-sub-title');
+    const carouselSubtitle = document.getElementById('ch-sub-counter');
 
-    setInterval(() => {
-        carousel.src = 'images/' + images[currentImage];
-        carouselTitle.innerHTML = images[currentImage];
-        if (currentImage === images.length - 1) {
-            currentImage = 0;
-        }
-        else {
-            currentImage++;
-        }
-    }, 2000);
+   if(currentImage === 33) {
+       currentImage = 1;
+   }
+    else {
+         currentImage++;
+    }
+
+    carousel.style.background = "linear-gradient(transparent, #1f1f1fd1), url(\"assets/images/case-history/" + currentImage + '.jpg") center/cover';
+    carouselTitle.innerHTML = 'Case History ' + currentImage;
+    carouselSubtitle.innerHTML = currentImage + ' di 33';
+}
+
+function previousCarousel() {
+    const carousel = document.getElementById('ch-sub');
+    const carouselTitle = document.getElementById('ch-sub-title');
+    const carouselSubtitle = document.getElementById('ch-sub-counter');
+
+    if(currentImage === 1) {
+        currentImage = 33;
+    }
+    else {
+        currentImage--;
+    }
+
+    carousel.style.background = "linear-gradient(transparent, #1f1f1fd1), url(\"assets/images/case-history/" + currentImage + '.jpg") center/cover';
+    carouselTitle.innerHTML = 'Case History ' + currentImage;
+    carouselSubtitle.innerHTML = currentImage + ' di 33';
 }
