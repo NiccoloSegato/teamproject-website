b5d3b6aeffb80a84aabc09ee6b78dd3d1ddc9a5b
niccolosegato <segato117@gmail.com>
2024-07-06 00:26:17 +0200

    UI and assets fixes

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -13,7 +13,7 @@ function nextCarousel() {
          currentImage++;
     }
 
-    carousel.style.background = "linear-gradient(transparent, #1f1f1fd1), url(\"assets/images/case-history/" + currentImage + '.jpg") center/cover';
+    carousel.style.background = "url(\"assets/images/case-history/" + currentImage + '.jpg") center/cover';
     carouselTitle.innerHTML = 'Case History ' + currentImage;
     carouselSubtitle.innerHTML = currentImage + ' di 33';
 }
